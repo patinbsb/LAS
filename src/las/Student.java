73bcc5645da95c4b1ca3321de9c023a086f330f4
alvinho0304 <alvinho0304@pc-143-61.customer.froomsgate.fusion.lan>
2017-03-05 22:53:29 +0000

    added Student class with codes from UML Class Model

---
+++ src/las/Student.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package las;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alvinho0304
+ */
+public class Student {
+
+    //Attitbutes
+    private String name;
+
+    private int studentID;
+
+    //Alvin : i'm not sure that am i right?
+    private ArrayList borrowedItems;
+    //ArrayList<String> borrowedItems = new ArrayList<>();
+    
+    private String email;
+
+    //Constructor
+    public Student(String name, int studentID, ArrayList borrowedItems, String email) {
+        this.name = name;
+        this.studentID = studentID;
+        this.borrowedItems = borrowedItems;
+        this.email = email;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList getBorrowedItems() {
+        return borrowedItems;
+    }
+
+    public void setBorrowedItems(ArrayList borrowedItems) {
+        this.borrowedItems = borrowedItems;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Methods
+    public void returnItem(Item book) {
+
+    }
+
+    public void issueBook(Item book) {
+
+    }
+}
