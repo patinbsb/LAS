189c943a6e16d16401c55341144571fdafab7a10
alvinho0304 <alvinho0304@pc-143-61.customer.froomsgate.fusion.lan>
2017-03-05 21:10:18 +0000

    added pseudo code inside those code and modified arguments from Member to an Object

--- src/las/SystemAdmin.java
+++ src/las/SystemAdmin.java
@@ -9,25 +9,40 @@ package las;
  *
  * @author Patrick Goodson
  */
-public class SystemAdmin extends Search
-{
-    public boolean addMember (Member newMember)
-    {
+public class SystemAdmin extends Search {
+
+    public boolean addMember(Object newmData) {
         // TODO implement this method
+        /**
+         * If Object mData belongs to a staff, connect to staff info system 
+         * else connect to student info system 
+         * Add a new member into corresponding info system
+         */
         return true;
     }
-    
-    public boolean removeMember(Member newMember)
-    {
+
+    public boolean removeMember(Object mData) {
         // TODO implement this method
+        /**
+         * If Object mData belongs to a staff, connect to staff info system;
+         * else connect to student info system; 
+         * Create for loop for corresponding member type of arraylist 
+         * if (listdata = mData) 
+         * remove member;
+         */
         return true;
     }
-    
-    public boolean changeMember(Member toChange, Member newMember)
-    {
+
+    public boolean changeMember(Object mChange, Object mData) {
         // TODO implement this method
+        /**
+         * If Object mData belongs to a staff, connect to staff info system;
+         * else connect to student info system; 
+         * Create for loop for corresponding member type of arraylist
+         * if(listdata = mData)
+         * change privilege of member with value stored in mChange;
+         */
         return true;
     }
-    
-    
+
 }
