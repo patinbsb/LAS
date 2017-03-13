61c53c70fb4dbf9572b916368aef6e867d2e6d2b
p2-goodson <p2-goodson@STU-CZC4143J91.campus.ads.uwe.ac.uk>
2017-03-13 19:37:47 +0000

    some other changes=

--- src/las/Issuer.java
+++ src/las/Issuer.java
@@ -11,16 +11,9 @@ public class Issuer extends Search
 {
     public void Issue(int ID, Item item)
     {
-        //TODO Implement this method
-        // Connect to staff + student database
-        // Get staff/student member to be issued
-        // Issue item
-        // if (StaffInfo.getStaffByID(ID).issueBook(bookID?))
-        // return true
-        // else
-        // return false
-        Object user = searchUserByID(ID);
-        //user.issueBook();
-        
+        Member user = searchMemberByID(ID);
+        user.issueItem(item);
     }
+    
+    
 }
