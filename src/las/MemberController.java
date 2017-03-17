1028c38a0c0f4355d446229e3932b8a96af92b69
alvinhippo <Shing2.Ho@live.uwe.ac.uk>
2017-03-17 18:26:55 +0000

    100% implementation on addingNewMember method

--- src/las/MemberController.java
+++ src/las/MemberController.java
@@ -5,74 +5,89 @@
  */
 package las;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author p2-goodson
  */
-public class MemberController
-{
+public class MemberController {
+
     public static MemberController memberController;
     static ArrayList<Member> memberList = new ArrayList<>();
-    static int currentMemberID = 0;
-    
-    private MemberController()
-    {
+    static int newMemberID = 0;
+    static int isStaffindex;
+
+    private MemberController() {
     }
-    
+
     // Using singleton pattern
-    public static MemberController getInstance()
-    {
-        if (memberController == null)
-        {
+    public static MemberController getInstance() {
+        if (memberController == null) {
             memberController = new MemberController();
         }
         return memberController;
     }
-    
-    public static void addNewMember(String name, String email, String privilege, boolean isStaff)
-    {
-        //method for getting last member ID then +1 to be a new ID
-        
-        memberList.add(new Member(currentMemberID, name, email, privilege, isStaff));
-        currentMemberID++;
+
+    public static void addNewMember(String name, String email, String privilege, boolean isStaff) {
+        try {
+            //method for getting last member ID then +1 to be a new ID\
+
+            String data = "SELECT MAX(MEMBER_ID) FROM LAS.MEMBERS";
+            PreparedStatement pt = DBConnector.conn.prepareStatement(data);
+
+            try (ResultSet rs = pt.executeQuery()) {
+                while (rs.next()) {
+                    newMemberID = rs.getInt(1) + 1;
+                }
+            }
+            //Converting a boolean into int for storing representating value 1 and 0 for true and false
+            if (isStaff) {
+                isStaffindex = 1;
+            } else {
+                isStaffindex = 0;
+            }
+            Member nMem = new Member(newMemberID, name, email, privilege, isStaff);
+            memberList.add(nMem);
+            DBConnector.insertMemberIntoTable(nMem);
+            System.out.println("New Member: " + name + " has been inserted into arraylist and table");
+        } catch (SQLException ex) {
+            Logger.getLogger(MemberController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
-    public static void removeMember(Member member)
-    {
+
+    public static void removeMember(Member member) {
         memberList.remove(member);
     }
-    
-    public static void changeMember(Member memberTochange, Member newMember)
-    {
+
+    public static void changeMember(Member memberTochange, Member newMember) {
         int memberPos = memberList.indexOf(memberTochange);
         memberList.set(memberPos, newMember);
     }
-    
-    public static Member getMemberByID(int ID)
-    {
-        for (Member member: memberList)
-        {
-            if (member.getID() == ID)
-            {
+
+    public static Member getMemberByID(int ID) {
+        for (Member member : memberList) {
+            if (member.getID() == ID) {
                 return member;
             }
         }
         return null;
     }
-    
-    public static ArrayList<Member> getMemberByName(String name)
-    {
+
+    public static ArrayList<Member> getMemberByName(String name) {
         ArrayList<Member> results = new ArrayList<>();
-        for (Member member: memberList)
-        {
-            if (member.getName().equals(name))
-            {
+        for (Member member : memberList) {
+            if (member.getName().equals(name)) {
                 results.add(member);
             }
         }
         return results;
     }
-    
+
 }
